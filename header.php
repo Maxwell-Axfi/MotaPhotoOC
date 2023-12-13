9574ea0e459fc164d0f3b102b049bcff57878b18
Maxwell-Axfi <lelia.vaultier@gmail.com>
2023-12-13 21:47:12 +0100

    Header.php complété, ajout du logo et correction du nom style.css

--- header.php
+++ header.php
@@ -8,8 +8,14 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-        <a class="navbar-brand" href="#"><?php bloginfo('name') ?></a>
-    </nav>
+    <header class="navbar">
+        <img class="navbar__logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/logo.svg'; ?>" alt="Nathalie Mota" />
+        <nav class="navbar__menu">
+            <ul class="navbar__list">
+                <li class="navbar__items">Accueil</li>
+                <li class="navbar__items">À propos</li>
+                <li class="navbar__items">Contact</li>
+            </ul> 
+        </nav>
 
     <div class="container">
