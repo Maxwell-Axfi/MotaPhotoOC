1193125a96eb12bb3371372e4d1e2280a295a5e0
Maxwell-Axfi <lelia.vaultier@gmail.com>
2023-12-18 12:51:13 +0100

    Image aléatoire dans le header de l'accueil

--- index.php
+++ index.php
@@ -14,13 +14,38 @@ $args = array(
 );
 
 $query = new WP_Query($args);
+
+// WP_Query pour récupérer une image aléatoire
+$arg_header = array(
+    'post_type'      => 'photo',
+    'posts_per_page' => 1,
+    'orderby'        => 'rand', // Ordre aléatoire
+);
+
+$query_header = new WP_Query($arg_header);
 ?>
 
 
-
 <div class="accueil__header">
-<h1 class="accueil__header-h1">Photographe event</h1>
-<img class="accueil__header-background" src="http://motaphoto.local/wp-content/uploads/2023/12/nathalie-11-scaled.jpg" />
+    <h1 class="accueil__header-h1">Photographe event</h1>
+
+    <?php // Vérifier si la requête a des posts
+    if ($query_header->have_posts()) {
+        // Boucler à travers les posts
+        while ($query_header->have_posts()) {
+            $query_header->the_post();
+
+            // Obtenir l'URL de l'image
+            $header_img_url = get_the_post_thumbnail_url();?>
+
+            <img class="accueil__header-background" src="<?php echo esc_url($header_img_url) ?>" />
+        
+        <?php }
+
+        // Réinitialiser les données du post
+        wp_reset_postdata();
+    }
+    ?>
 </div>
 
 <div class="accueil__photo-block">
